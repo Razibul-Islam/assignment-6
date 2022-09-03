bee8735f92d87dc0bf5566f6a4cf3579547fec55
Razibul Islam <razibulislam665@gmail.com>
2022-09-03 16:50:17 +0600

    all News added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const loadNews = async() => {
+const loadNews = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url)
     const data = await res.json();
@@ -8,18 +8,44 @@ const loadNews = async() => {
 const displayNews = news => {
     const ulParent = document.getElementById('new-nav');
     news.forEach(newsAll => {
-        const ulCreate = document.createElement('ul');
-        ulCreate.classList.add('navbar-nav', "fs-4");
-        ulCreate.innerHTML = `
-        <li class="nav-item">
-        <a class="nav-link" aria-current="page" href="${newsAll.category_name}">${newsAll.category_name}</a>
-      </li>
+        const liCreate = document.createElement('li');
+        liCreate.classList.add('nav-item');
+        liCreate.innerHTML = `
+        <a class="nav-link mx-3" aria-current="page" href="${newsAll.category_name}">${newsAll.category_name}</a>
         `;
-        ulParent.appendChild(ulCreate);
+        ulParent.appendChild(liCreate);
     })
 
 }
 
+const loadNewsContent = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`;
+    const res = await fetch(url);
+    const data = await res.json();
+    newContentNews(data.data);
+}
+
+const newContentNews = contentNew => {
+    const card = document.getElementById('card');
+    // console.log(contentNew);
+    contentNew.forEach(newContent => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+        <div class="card">
+            <img src="${newContent.image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${newContent.title}</h5>
+            <p class="card-text">${newContent.details}</p>
+        </div>
+        </div>
+        `;
+        card.appendChild(cardDiv)
+    });
+}
 
 
+
+
+loadNewsContent()
 loadNews()
